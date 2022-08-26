ddbb286484578128c0bdd72e141d14c2f394c17a
Alunos <aluno.etec@etecEmbu.com.br>
2022-08-25 21:25:54 -0300

    Iniciando a edição

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+console.log('Hello World'); //Cria comentario em linha reta
+/*
+Cria comentario em varias linhas
+*/
+
+let n1=5;
+let n2=6;
+let result= (n1+n2);
+alert(`O valor da soma é: ${result}`);
