c7dbf1c91f5d77d8f7c6402e7ddca56ee3862660
andreicsantana <andrei.santana72@gmail.com>
2022-09-15 21:08:17 -0300

    Portifolio atual

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,90 @@ console.log('Hello World'); //Cria comentario em linha reta
 /*
 Cria comentario em varias linhas
 */
+ 
+// //Soma
+// let n1=5;
+// let n2=6;
+// let soma = (n1+n2);
+// console.log(soma);
 
-let n1=5;
-let n2=6;
-let result= (n1+n2);
-alert(`O valor da soma é: ${result}`);
+// //Subtrair
+// let n3=5;
+// let n4=6;
+// let subtracao = (n3-n4);
+// console.log(subtracao);
+
+// //Multiplicar
+// let n5=5;
+// let n6=6;
+// let multiplicar = (n5*n6);
+// console.log(multiplicar);
+
+// //divisao
+// let n7=5;
+// let n8=6;
+// let divisao = (n7/n8);
+// console.log(divisao);
+
+// //Exponenciacao
+// let n9=5;
+// let n10=6;
+// let expoente = (n9**n10);
+// console.log(expoente);
+
+// //resto da divisao
+// let n11=5;
+// let n12=6;
+// let rdivisao= (n11 % n12);
+// console.log(rdivisao);
+
+// //acrescentar
+// let n13 =10;
+// console.log(++n13);
+
+// //decremento
+// let n14 =10;
+// console.log(--n13);
+
+// //comparacao
+// let n15 = 10
+// let n16 = 10
+// n15 == n16
+// console.log()
+
+//Exemplos operadores de comparação
+//Maior >
+
+// let n1 = 20;
+// let n2 = 14;
+// console.log(n1 > n2);
+
+// let n3 = 20;
+// let n4 = 14;
+// console.log(n3 >= n4);
+
+// let n5 = 20;
+// let n6 = 14;
+// console.log(n5 < n6);
+
+// let n7 = 20;
+// let n8 = 14;
+// console.log(n7 <= n8);
+
+// let n9 = 20;
+// let n10 = 14;
+// console.log(n9 == n10);
+
+// let n11 = 20;
+// let n12 = 14;
+// console.log(n11 != n12);
+
+// let n13 = 20;
+// let n14 = 14;
+// console.log(n13 !== n14);
+
+
+
+
+
+
